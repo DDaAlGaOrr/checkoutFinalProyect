27c516baad9f72722fb4fbef78667b827e769370
DDaAlGaOrr <daalgaor.17@gmail.com>
2022-04-27 11:09:43 -0700

    products print in caja page

--- public/js/app.js
+++ public/js/app.js
@@ -8,21 +8,32 @@ let form = document.getElementById('form')
 const feedDisplay = document.querySelector('#feed')
 const strategyManager = new StrategyManager
 /* --------------------------------------------------------------------------------------------------------- */
-fetch('http://localhost:3000/data',)
+fetch('http://localhost:3000/data', )
     .then(response => response.json())
     .then(data => {
         console.log(data)
         data.forEach(element => {
-            const card = `<h2>Nombre: ${element.nombre} Precio: ${element.precio_venta} Peso:${element.cantidad}${element.medida}</h2> `
-            feedDisplay.insertAdjacentHTML("beforeend",card)
+            const card = `
+                              <div class="col-lg-4 mb-3">
+                                    <div class="card h-100">
+                                        <img src="..." class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                        <h5 class="card-title">${element.nombre}</h5>
+                                        <p class="card-text">Precio: ${element.precio_venta}</p>
+                                        <a href="" class=" btn btn-primary mt-2">Ver detalles</a>
+                                        <!-- <a href="" class=" btn btn-success mt-2">Añadir al carrito </a> -->
+                                        <a href="" class=" btn btn-success mt-2">Comprar</a>
+                                    </div>
+                                </div>
+                            </div><!-- /.col-lg-4 -->`
+            feedDisplay.insertAdjacentHTML("beforeend", card)
         });
     })
 /* --------------------------------------------------------------------------------------------------------- */
-    form.addEventListener("submit", (e) => {
-        e.preventDefault()
-        // console.log(datos)
-        const loginForm = new LoginForm
-        strategyManager.strategy = loginForm
-        strategyManager.doAction(form)
-    })    
-
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    // console.log(datos)
+    const loginForm = new LoginForm
+    strategyManager.strategy = loginForm
+    strategyManager.doAction(form)
+})
